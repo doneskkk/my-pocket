77f171092c9cf6768b26ce3b7d02b65f0b11ab31
Chiril Dones <doneskirill3@gmail.com>
2023-12-29 10:43:49 +0200

    add budget service

--- src/main/java/com/donesk/moneytracker/service/BudgetService.java
+++ src/main/java/com/donesk/moneytracker/service/BudgetService.java
@@ -1,11 +1,11 @@
 package com.donesk.moneytracker.service;
 
-import com.donesk.moneytracker.entity.Budget;
-import com.donesk.moneytracker.entity.Transaction;
-import com.donesk.moneytracker.entity.User;
+import com.donesk.moneytracker.model.Budget;
+import com.donesk.moneytracker.model.User;
 import com.donesk.moneytracker.exception.BudgetNotFoundException;
 import com.donesk.moneytracker.repository.BudgetRepo;
 import com.donesk.moneytracker.repository.TransactionRepo;
+import com.donesk.moneytracker.repository.UserRepo;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,37 +17,30 @@ public class BudgetService {
 
     private final BudgetRepo budgetRepo;
     private final TransactionRepo transactionRepo;
-    private final UserService userService;
+    private final UserRepo userRepo;
 
 
-    public BudgetService(BudgetRepo budgetRepo, TransactionRepo transactionRepo, UserService userService) {
+    public BudgetService(BudgetRepo budgetRepo, TransactionRepo transactionRepo, UserService userService, UserRepo userRepo) {
         this.budgetRepo = budgetRepo;
         this.transactionRepo = transactionRepo;
-        this.userService = userService;
+        this.userRepo = userRepo;
     }
 
     @Transactional
     public Long create(Budget budget, Principal principal){
-        User authenticatedUser = userService.findByUsername(principal.getName()).get();
-
+        User authenticatedUser = userRepo.findByUsername(principal.getName()).get();
         budget.setUser(authenticatedUser); // Associate the authenticated user with the budget
-
         return budgetRepo.save(budget).getId();
     }
 
     public Budget getBudget(Long id){
         Budget budget = budgetRepo.findById(id).orElseThrow( () -> new BudgetNotFoundException("Budget with id "+ id + " wasn't found"));
-
         return budget;
     }
 
     @Transactional
-    public void updateBudget(Long budgetId, Transaction transaction){
-        Budget budget = budgetRepo.findById(budgetId).orElseThrow( () -> new BudgetNotFoundException("Budget with id "+ budgetId + " wasn't found"));
-        double transactionAmount = transaction.getAmount();
-        budget.updateCurrentProgress(transactionAmount);
-        System.out.println(budget.getCurrentProgress());
-        if(isGoalAchieved(budgetId)){
+    public void updateStatus(Budget budget){
+        if(isGoalAchieved(budget.getId())){
             budget.setStatus(true);
         }
         budgetRepo.save(budget);
@@ -57,11 +50,16 @@ public class BudgetService {
         return budgetRepo.findAll();
     }
 
-
-
-
     private boolean isGoalAchieved(Long budgetId){
         Budget budgetToCheck = budgetRepo.findById(budgetId).get();
         return budgetToCheck.getGoal() <= budgetToCheck.getCurrentProgress();
     }
+
+
+    public List<Budget> getBudgetsByCreatedBy(String username) {
+        User user = userRepo.findByUsername(username).get();
+        List<Budget> budgets = budgetRepo.findByUserId(user.getId());
+        return budgets;
+    }
+
 }

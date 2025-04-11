ae3e940a1802e6fcf62388c42b269ad5d09ce909
Chiril Dones <doneskirill3@gmail.com>
2025-04-11 12:21:02 +0300

    fix: remove unnecessary endpoint

--- src/main/java/com/donesk/moneytracker/controller/UserController.java
+++ src/main/java/com/donesk/moneytracker/controller/UserController.java
@@ -15,12 +15,4 @@ public class UserController {
         this.budgetService = budgetService;
     }
 
-//    @GetMapping("/{username}/budgets")
-//    public ResponseEntity<List<BudgetDTO>> getBudgetsCreatedBy(@PathVariable(value = "username") String username,
-//                                                                     @RequestParam(value = "page", required = false, defaultValue = Constants.DEFAULT_PAGE_NUMBER) Integer page,
-//                                                                 @RequestParam(value = "size", required = false, defaultValue = Constants.DEFAULT_PAGE_SIZE) Integer size) {
-//        List<BudgetDTO> response = budgetService.getBudgetsByCreatedBy(username, page, size);
-//
-//        return new ResponseEntity<>(response, HttpStatus.OK);
-//    }
 }
